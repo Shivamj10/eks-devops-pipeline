7698e7a2c65d87cb3a72e8fd30bb4b0c7f0609f7
Shivamj10 <shivamjugran10@gmail.com>
2025-04-22 00:32:15 +0530

    Add VPC Terraform module

---
+++ terraform/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "project_name" {
+  description = "Name prefix for resources"
+  type        = string
+  default     = "eks-pipeline"
+}
+
