0a098b99c215750543595f915083fb88ad0336dd
Shivamj10 <shivamjugran10@gmail.com>
2025-04-24 00:04:09 +0530

    subnet

---
+++ subnet/variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of public subnet CIDR blocks"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet CIDR blocks"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of AZs for subnets"
+}
+
